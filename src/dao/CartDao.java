b201efbbca429362664536adcbcef3ba0b1d581f
navreet10 <kaur.navreet1090@gmail.com>
2016-07-15 13:55:39 -0400

    Navreet Cart changes

--- src/dao/CartDao.java
+++ src/dao/CartDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -16,80 +17,92 @@ public class CartDao {
 
 	public static void insertCart(Cart cart) {
 		EntityManager em = DBUtil.getEmFactory().createEntityManager();
-        EntityTransaction trans = em.getTransaction();
-        try {
-            trans.begin();
-            em.persist(cart);
-            trans.commit();
-        } catch (Exception e) {
-            trans.rollback();
-        } finally {
-            em.close();
-        }		
+		EntityTransaction trans = em.getTransaction();
+		try {
+			trans.begin();
+			em.persist(cart);
+			trans.commit();
+		} catch (Exception e) {
+			trans.rollback();
+		} finally {
+			em.close();
+		}
 	}
 
 	public static Product getProduct(String prodId) {
 		EntityManager em = DBUtil.getEmFactory().createEntityManager();
-        String qString = "select b from product b where b.prodid = :prodId";
-        
-        Product prod = new Product();
-        try{
-            TypedQuery<Product> query = em.createQuery(qString,Product.class);
-            query.setParameter("prodId", Long.parseLong(prodId));
-            prod = query.getSingleResult();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        finally{
-                em.close();
-            }
+		String qString = "select b from Product b where b.prodid = :prodId";
+
+		Product prod = new Product();
+		try {
+			TypedQuery<Product> query = em.createQuery(qString, Product.class);
+			query.setParameter("prodId", Long.parseLong(prodId));
+			prod = query.getSingleResult();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
+		}
 		return prod;
 	}
 
 	public static List<Cart> getCartItems() {
 		EntityManager em = DBUtil.getEmFactory().createEntityManager();
-        List<Cart> items = null;
-        String qString = "select b from cart b";
-        
-        try{
-            TypedQuery<Cart> query = em.createQuery(qString,Cart.class);
-            
-            items = query.getResultList();
-            
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        finally{
-                em.close();
-            }
-        return items;  
+		List<Cart> items = null;
+		String qString = "select b from Cart b";
+
+		try {
+			TypedQuery<Cart> query = em.createQuery(qString, Cart.class);
+
+			items = query.getResultList();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
+		}
+		return items;
 	}
 
 	public static void order(List<Cart> cartUpdated, String username) {
 		EntityManager em = DBUtil.getEmFactory().createEntityManager();
-		try{
-			String qString = "select b from userorder b where userId = "
-					+ "(select a.userid from minionuser a where username = :uaername) ";
-			TypedQuery<Userorder> query = em.createQuery(qString,Userorder.class);
+		try {
+			String qString = "select b from Userorder b where b.minionuser.userid = "
+					+ "(select a.userid from minionuser a where a.username = :username) ";
+			TypedQuery<Userorder> query = em.createQuery(qString, Userorder.class);
+			query.setParameter("username", username);
 			Userorder userorder = query.getSingleResult();
-			long orderid = userorder.getUserorderid() +1;
-		for (Cart cart: cartUpdated) {
-	       Minionorder order = new Minionorder();	       
-	       order.setOrdername(username+orderid);
-	       order.setProduct(cart.getProduct());
-	       order.setQtty(cart.getQtty());
-	       EntityTransaction trans = em.getTransaction();
+			long orderid = userorder.getUserorderid() + 1;
+			for (Cart cart : cartUpdated) {
+				Minionorder order = new Minionorder();
+				order.setOrdername(username + orderid);
+				order.setProduct(cart.getProduct());
+				order.setQtty(cart.getQtty());
+				EntityTransaction trans = em.getTransaction();
+				try {
+					trans.begin();
+					em.persist(order);
+					trans.commit();
+				} catch (Exception e) {
+					trans.rollback();
+				}
+			}
+			userorder.setOrdercount(new BigDecimal(orderid));
+			EntityTransaction trans = em.getTransaction();
+			try {
+				trans.begin();
+				em.merge(userorder);
+				trans.commit();
+			} catch (Exception e) {
+				trans.rollback();
+			}
+			em.createQuery("DELETE FROM Cart").executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
 		}
-		
-        
-            
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        finally{
-                em.close();
-            }
-		
+
 	}
 
 }
